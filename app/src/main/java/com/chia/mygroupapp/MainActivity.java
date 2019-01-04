5274c6778a4c67d5eda3fd15697cd1ffedfc0269
aceg224 <aceg224@gmail.com>
2019-01-04 15:45:28 +0800

    test1526

--- app/src/main/java/com/chia/mygroupapp/MainActivity.java
+++ app/src/main/java/com/chia/mygroupapp/MainActivity.java
@@ -9,13 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Change01
-        //change02
-        //1504_Hsieh
-        //1504_Hsieh_2
-//        1504_Hsieh_3
-//        1504_Hsieh_4
-        //        1504_Hsieh_5
-
+        //Hsieh_09
     }
 }
