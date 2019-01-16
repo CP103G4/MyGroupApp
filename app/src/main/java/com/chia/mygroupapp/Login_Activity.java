5f3c0003f59ddc7b90d41a9f58caa2408eb08a07
k86095 <k86095@gmail.com>
2019-01-16 18:23:30 +0800

    login

--- app/src/main/java/com/chia/mygroupapp/Login_Activity.java
+++ app/src/main/java/com/chia/mygroupapp/Login_Activity.java
@@ -1,19 +1,40 @@
 package com.chia.mygroupapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class Login_Activity extends AppCompatActivity {
+
+    EditText etAccount;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        handleViews();
 
     }
 
+    private void handleViews() {
+        etAccount = findViewById(R.id.etAccount);
+        etPassword =findViewById(R.id.etPassword);
+    }
+
+
+    public void onLogInClick(View view) {
+        Intent loginIntent = new Intent(this, MainActivity.class);
+        startActivityForResult(loginIntent,0);
+    }
+
+    public void onIgnoreClick(View view) {
+        Intent loginIntent = new Intent(this, MainActivity.class);
+        startActivityForResult(loginIntent,0);
+    }
 }
--- app/src/main/java/com/chia/mygroupapp/MainActivity.java
+++ app/src/main/java/com/chia/mygroupapp/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handleViews();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -46,5 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void handleViews() {
     }
 }
